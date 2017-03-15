1a49c727174b85c8e74e8b8b3823cdb340db2b29
Kelan Riley <kelan.riley@ndsu.edu>
2017-03-15 14:47:41 -0500

    Adding a default target to the build file and doing some reorganzing of the documentation.

---
+++ src/Position.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public interface Position<E> {
+  //Get the element stored at this position.
+  public E getElement();
+}
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * This class should run all the tests for various data structures that I
+ * build in making the game.
+ */
+public class Test {
+  public static void main(String[] args) {
+    TestGraph.testGraph();
+  }
+}
